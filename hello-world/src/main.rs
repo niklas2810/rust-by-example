32e03fd3ef444a960cd6748fb8c1c90dbefef980
Niklas Arndt <git@niklasarndt.com>
2021-07-06 11:29:15 +0200

    Part 2: Primitives

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -3,7 +3,7 @@ use core::fmt;
 #[derive(Debug)]
 struct Complex {
     real: f64,
-    imag: f64
+    imag: f64,
 }
 
 impl fmt::Display for Complex {
@@ -18,8 +18,16 @@ fn main() {
     println!("{} days", 31);
     println!("As a {0}, we can print {0} twice", "Test");
     println!("Binary number: {:b}", 9);
-    println!("Let's print a number: {content:>0min_width$}", content=42, min_width=3);
-    println!("..or indent text: {content:>min_width$}", content="Test", min_width=10);
+    println!(
+        "Let's print a number: {content:>0min_width$}",
+        content = 42,
+        min_width = 3
+    );
+    println!(
+        "..or indent text: {content:>min_width$}",
+        content = "Test",
+        min_width = 10
+    );
 
     #[derive(Debug)]
     struct Structure(i32, i32);
@@ -35,13 +43,15 @@ fn main() {
     #[derive(Debug)]
     struct Deep(Structure);
 
-    println!("Here's a deep struct: {:?}", Deep(Structure(1,2)));
+    println!("Here's a deep struct: {:?}", Deep(Structure(1, 2)));
 
     let pi = 3.141592;
     println!("Pi is roughly {:.3}", pi);
 
-
-    let num = Complex{real: 3.3, imag: 7.2};
+    let num = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
 
     println!("Display: {}", num);
     println!("Debug: {:?}", num);
